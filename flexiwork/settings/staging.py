2eab8108cde6ae96922de2194edb736b8dd88b06
rahman <rahman.s@e360africa.com>
2020-04-09 11:54:22 +0100

    Included logging to staging settings

--- flexiwork/settings/staging.py
+++ flexiwork/settings/staging.py
@@ -1,7 +1,9 @@
 from datetime import timedelta
 from .base import *
 
-ALLOWED_HOSTS = ['energy360africa.com']
+STAGING_BASE_DIR = '/var/www/PYTHON/flexiwork_backend'
+
+ALLOWED_HOSTS = ['.energy360africa.com']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -16,5 +18,24 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(hours=6)
 }
 
-MEDIA_ROOT = '/var/www/PYTHON/flexiwork_backend/media'
+MEDIA_ROOT = os.path.join(STAGING_BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+LOGGING = {                                                                                                                 
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'logfile': {
+            'class': 'logging.RotatingFileHandler',
+            'filename': os.path.join(STAGING_BASE_DIR, 'server.log'),
+            'maxBytes': 1024*1024*15, # 15MB
+            'backupCount': 10,
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['logfile'],
+            'level': 'INFO'
+        },
+    },
+}
